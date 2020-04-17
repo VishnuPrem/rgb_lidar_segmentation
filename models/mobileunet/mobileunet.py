6418742b84d0970d9e7226e182107ae52574f3fa
rodri651 <rodri651@seas.upenn.edu>
2020-04-16 20:10:23 -0400

    resunet: just for fun

---
+++ models/mobileunet/mobileunet.py
@@ -0,0 +1,112 @@
+import pdb
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+def convrelu(in_channels, out_channels, kernel, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
+
+class Net(nn.Module):
+    def __init__(self, CHANNELS,NUM_CLASSES):
+        super(Net,self).__init__()
+
+        self.base_model = models.mobilenet_v2(pretrained=True)
+        self.base_layers = list(self.base_model.children())
+        
+        self.base_layers = self.base_layers[0]
+        #print(self.base_layers[0])
+        #self.base_layers[0] = nn.Conv2d(CHANNELS,64,kernel_size=(7,7),stride=(2,2),padding=(3,3),bias=False)
+        #print(self.base_layers[0])
+        self.layer0 = nn.Sequential(*self.base_layers[:2]) # size=(N, 64/16, x.H/2, x.W/2)
+        self.layer0_1x1 = convrelu(16,16, 1, 0)
+        self.layer1 = nn.Sequential(*self.base_layers[2:4]) # size=(N, 64/24, x.H/4, x.W/4)
+        self.layer1_1x1 = convrelu(24, 24, 1, 0)
+        self.layer2 = nn.Sequential(*self.base_layers[4:7]) # size=(N, 128/32, x.H/8, x.W/8)
+        self.layer2_1x1 = convrelu(32, 32, 1, 0)
+        self.layer3 = nn.Sequential(*self.base_layers[7:11])  # size=(N, 256/64, x.H/16, x.W/16)
+        self.layer3_1x1 = convrelu(64, 64, 1, 0)
+        self.layer4 = nn.Sequential(*self.base_layers[11:])  # size=(N, 512/1280, x.H/32, x.W/32)
+        self.layer4_1x1 = convrelu(1280, 1280, 1, 0)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        
+        self.conv_up3 = convrelu(64 + 1280, 1280, 3, 1)
+        self.conv_up2 = convrelu(32 + 1280, 512, 3, 1)
+        self.conv_up1 = convrelu(24 + 512, 256, 3, 1)
+        self.conv_up0 = convrelu(16 + 256, 128, 3, 1)
+
+        self.conv_original_size0 = convrelu(CHANNELS, 16, 3, 1)
+        self.conv_original_size1 = convrelu(16, 16, 3, 1)
+        self.conv_original_size2 = convrelu(16 + 128, 16, 3, 1)
+
+        self.conv_last = nn.Conv2d(16, NUM_CLASSES, 1)
+
+    def forward(self, input, x2 ,mask):
+        x_original = self.conv_original_size0(input)
+        x_original = self.conv_original_size1(x_original)
+
+        layer0 = self.layer0(input)
+        print('x_original',x_original.shape, 'layer0',layer0.shape)
+        layer1 = self.layer1(layer0)
+        
+        layer2 = self.layer2(layer1)
+        
+        layer3 = self.layer3(layer2)
+        
+        layer4 = self.layer4(layer3)
+        
+        layer4 = self.layer4_1x1(layer4)
+        print('lowest feature size',layer4.shape)
+        x = self.upsample(layer4)
+        layer3 = self.layer3_1x1(layer3)
+
+        print('first concat',x.shape,layer3.shape)
+        x = torch.cat([x, layer3], dim=1)
+        x = self.conv_up3(x)
+
+        x = self.upsample(x)
+
+        layer2 = self.layer2_1x1(layer2)
+        print('second concat',x.shape,layer2.shape)
+        x = torch.cat([x, layer2], dim=1)
+        x = self.conv_up2(x)
+
+        x = self.upsample(x)
+        layer1 = self.layer1_1x1(layer1)
+        print('third concat',x.shape,layer1.shape)
+        x = torch.cat([x, layer1], dim=1)
+        x = self.conv_up1(x)
+
+        x = self.upsample(x)
+        layer0 = self.layer0_1x1(layer0)
+        print('fourth concat',x.shape,layer0.shape)
+        x = torch.cat([x, layer0], dim=1)
+        x = self.conv_up0(x)
+
+        x = self.upsample(x)
+
+        x = torch.cat([x, x_original], dim=1)
+        x = self.conv_original_size2(x)
+
+        out = self.conv_last(x)
+
+        return out
+
+def unit_test():
+    num_minibatch = 2
+    num_channels = 3
+    rgb = torch.randn(num_minibatch, num_channels, 64, 512).cuda(0)
+    
+    rtf_net = Net(num_channels,4).cuda(0)
+    
+    output=rtf_net(rgb,rgb,rgb)
+    print(output.shape)
+    
+
+if __name__ == '__main__':
+    unit_test()
